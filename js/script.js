636f8257d7d684317d61446b53b78d2cbadaa87e
KristiancDev <kristiancdev@gmail.com>
2024-05-30 09:44:19 -0500

    Rediseño web

--- js/script.js
+++ js/script.js
@@ -1,84 +1,40 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const hamburger = document.querySelector(".hamburger");
-  const navLinks = document.querySelector(".nav-links");
+  // Elementos del DOM
+  const menuToggle = document.querySelector(".menu-toggle");
+  const menu = document.querySelector(".menu");
+  const themeToggle = document.querySelector(".theme-toggle");
+  const body = document.body;
 
-  hamburger.addEventListener("click", () => {
-    const expanded =
-      hamburger.getAttribute("aria-expanded") === "true" || false;
-    hamburger.setAttribute("aria-expanded", !expanded);
-    navLinks.classList.toggle("active");
+  /**
+   * Toggle del menú responsive.
+   */
+  menuToggle.addEventListener("click", function () {
+    menu.classList.toggle("show");
+  });
+
+  /**
+   * Cierra el menú al hacer clic en un enlace del menú.
+   */
+  document.querySelectorAll(".menu a").forEach((item) => {
+    item.addEventListener("click", function () {
+      menu.classList.remove("show");
+    });
+  });
+
+  /**
+   * Toggle del tema claro/oscuro.
+   */
+  themeToggle.addEventListener("click", function () {
+    body.classList.toggle("dark");
   });
 });
 
-document.addEventListener("DOMContentLoaded", function (event) {
-  var dataText = [
-    "Entusiasta por el desarrollo",
-    "Apasionado por la tecnología",
-    "Freelancer Creativo",
-  ];
-
-  function typeWriter(text, i, fnCallback) {
-    if (i < text.length) {
-      document.querySelector(".typed-words").innerHTML =
-        text.substring(0, i + 1) + '<span aria-hidden="true"></span>';
-
-      setTimeout(function () {
-        typeWriter(text, i + 1, fnCallback);
-      }, 100);
-    } else if (typeof fnCallback == "function") {
-      setTimeout(fnCallback, 700);
-    }
-  }
-
-  function StartTextAnimation(i) {
-    if (typeof dataText[i] == "undefined") {
-      setTimeout(function () {
-        StartTextAnimation(0);
-      }, 20000);
-    }
-    if (i < dataText[i].length) {
-      typeWriter(dataText[i], 0, function () {
-        StartTextAnimation(i + 1);
-      });
-    }
-  }
-  StartTextAnimation(0);
+// Smooth scroll for anchor links
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
 });
-
-function filterSelection(c) {
-  var x, i;
-  x = document.getElementsByClassName("portfolio-item");
-  if (c == "all") c = "";
-  for (i = 0; i < x.length; i++) {
-    w3RemoveClass(x[i], "show");
-    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
-  }
-}
-
-function w3AddClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    if (arr1.indexOf(arr2[i]) == -1) {
-      element.className += " " + arr2[i];
-    }
-  }
-}
-
-function w3RemoveClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    while (arr1.indexOf(arr2[i]) > -1) {
-      arr1.splice(arr1.indexOf(arr2[i]), 1);
-    }
-  }
-  element.className = arr1.join(" ");
-}
-
-// Añade clase 'show' a los elementos filtrados al cargar la página
-window.onload = function () {
-  filterSelection("all");
-};
